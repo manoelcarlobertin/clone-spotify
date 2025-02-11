38038045c5c4ee890bee30bda119a372f4e20bc1
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-02-11 16:32:17 -0300

    criada a pasta components, o Header e alguns comentários.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
-function App() {
+import React from "react";
 
-  return <h1>Hello, World!</h1>;
+const App = () => {
+  return <div>App</div>;
 }
 
 export default App
+
+// rafce
+
+//Arrow function
+// const App = () => <h1>Olá Mundo!</h1>;
+// PascalCase como eu nomeio do meu compontente
+
+// export function, posso importar com qq nome e sem chaves
+// export "sem default", só posso importar entre chaves e com o nome exportado
+
+
