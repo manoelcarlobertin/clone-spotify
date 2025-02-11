7eb142f32355d8e423a022b12e4c82f22915d791
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-02-11 17:19:17 -0300

    aaté aqui tá mostrando certo na tela falta ajustar header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+// import React from "react";
 import logoSpotify from "../assets/logo/spotify-logo.png";
 
 const Header = () => {
   return (
     <div>
       <img src={logoSpotify} alt="Logo do Spotify" />
-      
+
       <h1>Spotify</h1>
-      <a
+      <p>Sua plataforma de música</p>
+      <button>Iniciar Sessão</button>
+      <button>Criar Conta</button>
+      <button>Ajuda</button>
+      <button>Conectar-se com outros aplicativos</button>
+      <button>Área Restrita</button>
+      <button>Sair</button>
     </div>
   );
 };
